49fcb09716571dcd4bf8b9472bfb51ecf0f9d5f2
Juliana Mesquita <60270373+JulianaMariaSousaMesquita@users.noreply.github.com>
2021-02-27 14:49:31 -0300

    Implementação classe boleto

--- src/boleto/Boleto.java
+++ src/boleto/Boleto.java
@@ -1,5 +1,43 @@
 package boleto;
 
 public class Boleto {
+	private int codigo;
+	private Date dataVencimento;
+	private double valorPago;
+	
+	public Boleto(int codigo, Date dataVencimento, double valorPago) {
+		super();
+		this.codigo = codigo;
+		this.dataVencimento = dataVencimento;
+		this.valorPago = valorPago;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
 
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+
+	public double getValorPago() {
+		return valorPago;
+	}
+
+
+	public void setValorPago(double valorPago) {
+		this.valorPago = valorPago;
+	}
 }
